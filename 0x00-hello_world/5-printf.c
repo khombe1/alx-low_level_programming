ebd0650e833b7ff11877917a66fbd4aa9e590ffe
kingsley <kingsleyba@gmail.com>
2023-07-13 10:47:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints  line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
