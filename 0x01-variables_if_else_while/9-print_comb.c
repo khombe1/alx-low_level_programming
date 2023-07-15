e626c2b3420f9c78012c4a3a84d1879bd4ffb9a2
kingsley <kingsleyba@gmail.com>
2023-07-15 10:03:49 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,19 @@
  * Return: Always 0 (success)
  */
 
-
 int main(void)
-{
-int d;
-for (d = 0; d < 10; d++)
-putchar(d + '0');
-if (d < 9)
-putchar(',');
-putchar(' ');
-putchar('\n');
-return (0);
-}
+	{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d + '0');
+		if (d < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
