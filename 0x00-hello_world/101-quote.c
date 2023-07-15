f5265842078df68a9fbae02f3c10e44911eb5a4a
kingsley <kingsleyba@gmail.com>
2023-07-15 14:42:57 -0700

    100-print_comb3.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that prints all line to standard error
- * Return 1 success)
+ * main - Entry point
+ * Return: always 1 (success)
  */
-inte main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return(1);
-}
+int main(void)
+{     
+     
+	rite(1," and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}                
+~ 
