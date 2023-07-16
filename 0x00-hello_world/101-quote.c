46837c0280a1561c1300cfa693fa9ebfd4fc6e88
kingsley <kingsleyba@gmail.com>
2023-07-16 11:20:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
- * Return: always 1 (success)
- */
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
-{     
-     
-	rite(1," and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-
-}                
-~ 
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}	
