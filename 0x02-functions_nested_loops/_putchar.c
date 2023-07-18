0d129d851d75b2e5189230221a69c9a34f893f75
kingsley <kingsleyba@gmail.com>
2023-07-18 14:56:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
-
 /**
- * _putchar - wrtite the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
+ *
  * Return: On success 1.
- * On error, -1 is returned, and the error is set approprietly.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _Putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
