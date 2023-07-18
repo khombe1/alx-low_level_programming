a153bd5b6dff49a5f8518c2b74e2ac9fced661c0
kingsley <kingsleyba@gmail.com>
2023-07-18 16:22:19 -0700

    5-sign.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,18 @@
 include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase*
  * Return: Always 0
- */void print_alphabet_x10(void);
+ */
+
+void print_alphabet_x10(void);
 {
-	int i;
-	int j;
-
-	for (j = 1; i <= 10; i++)
-	{
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+int i;
+int j;
+for (j = 1; i <= 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
 }	
-
-
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
  * _islower - shows 1 if the input is a
- * lowercase character. Another cases, shows
- * 0
- *
- * @c: The character in the ASCII code
- *
+ * lowercase character. Another cases, shows 0
+ *  @c: The character in the ASCII code
  * Return: 1 for lower case character. 0 for the rest.
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0)
-	}
-	_putchar('\n');
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
 }
 	
