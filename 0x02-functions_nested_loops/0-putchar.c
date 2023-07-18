09f75f8cffbe58f9238c02ea1d3a8f71fbae89d9
kingsley <kingsleyba@gmail.com>
2023-07-18 12:38:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+print("_putchar\n");
+return (0);
+}
