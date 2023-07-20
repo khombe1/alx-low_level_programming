82b03db53d621b61c293fed26446916dbf1e66b1
kingsley <kingsleyba@gmail.com>
2023-07-20 08:04:54 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isupper - write letter c to stdout
+ * @c: the letter to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set
+ * 
+ */
+
+int _putchar(char c);
+{
+	return (write(1, &C, 1));
+}
