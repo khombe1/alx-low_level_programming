402eda6b04aafdad8559cc8513e46403e0a35fee
kingsley <kingsleyba@gmail.com>
2023-07-26 11:20:59 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int length_of_string, z;
-/* z is acounter for n bytes of src to be concatenated */
-/* length-of_string = length of destination string */
+int length_of_string, z;
 
 	length_of_string = 0;
 	while (dest[length_of_string] != '\0')
 	{
-		length_of_string++;
+	length_of_string++;
 	}
+
 	for (z = 0; z < n && src[z] != '\0'; z++, length_of_string++)
 	{
-	dest([length_of_string] = src[z];
+	dest[length_of_string] = src[z];
 	}
 	dest[length_of_string] = '\0';
 	return (dest);
