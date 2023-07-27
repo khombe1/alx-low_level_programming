4029ed3f1f16e3ebc93ea6db12110c1beba11e3a
kingsley <kingsleyba@gmail.com>
2023-07-27 03:04:40 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program.
- *
- * This function initializes an array, modifies its element using a pointer,
- * and then prints the modified value.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	int n;
@@ -15,9 +7,17 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &a[2];
-	*p = 98;
-	/* ---so that this prints 98\n */
+	p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+	;
+  /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
