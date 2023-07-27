9cdd1e6e9e927d5cc8fb6dd59b6578f14dcfa8d9
kingsley <kingsleyba@gmail.com>
2023-07-27 02:52:49 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function initializes an array, modifies its element using a pointer,
+ * and then prints the modified value.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &a[2];
+	*p = 98;
+	/* ---so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
