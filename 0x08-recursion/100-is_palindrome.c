8d32e3089d9dadd637fe4f07d10183b0faead045
kingsley <kingsleyba@gmail.com>
2023-08-02 14:11:10 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,34 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 if it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
 /**
  * check_pal - checks the characters recursively for palindrome
  * @s: string to check
@@ -6,36 +37,6 @@
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, int i, int len);
-
-/**
- * _strlen_recursion - return the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s);
-
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 if not
- */
-int is_palindrome(char *s)
-{
-	if (_strlen_recursion(s) <= 1)
-	return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
-
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * actual_prime - calculate if a number is a prime number recursively
  * @n: number to evaluate
@@ -13,6 +15,7 @@ int actual_prime(int n, int i);
  *
  * Return: 1 if n is a prime number, 0 if not
  */
+int is_prime_number(int n);
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -25,6 +28,6 @@ int actual_prime(int n, int i)
 	if (i == 1)
 	return (1);
 	if (n % i == 0 && i > 0)
-	return (0)
+	return (0);
 	return (actual_prime(n, i - 1));
 }
