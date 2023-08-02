032e98910222f7854a7a3523075ee04e697d5626
kingsley <kingsleyba@gmail.com>
2023-08-02 11:20:43 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+/**
+ * wildcmp - Compare strings with wildcards
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string with wildcards
+ * Return: 1 if the strings match, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+	if (*s2 != '\0' && *s2 == '*')
+	return (wildcmp(s1, s2 + 1));
+	return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+	return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
