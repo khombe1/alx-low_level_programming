bc4da1c81e4a9feecf9b5cc8d9483a6a30538b42
kingsley <kingsleyba@gmail.com>
2023-08-04 09:35:46 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,55 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * check_num - check if there are digits in the string
- * @str: input string
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: arguments
  *
- * Return: 1 if no digit found, 0 if at least one digit is found
- */
-int check_num(char *str)
-{
-	unsigned int count = 0;
-
-	while (count < strlen(str))
-	{
-	if (isdigit(str[count]))
-	{
-	return (0);
-	}
-	count++;
-	}
-	return (1);
-}
-
-/**
- * main - print the sum of numbers provided as command-line arguments
- * @argc: count of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success, 1 on error
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int i, n, sum = 0;
+	char *flag;
 
-	count = 1;
-	while (count < argc)
+	if (argc < 2)
 	{
-	if (check_num(argv[count]))
-	{
-	str_to_int = atoi(argv[count]);
-	sum += str_to_int;
+	printf("0\n");
+	return (0);
 	}
-	else
+
+	for (i = 1; i < argc; i++)
+	{
+	n = (int)strtol(argv[i], &flag, 10);
+	if (*flag)
 	{
 	printf("Error\n");
 	return (1);
 	}
-	count++;
+	else
+	{
+	sum += n;
+	}
 	}
 	printf("%d\n", sum);
 	return (0);
