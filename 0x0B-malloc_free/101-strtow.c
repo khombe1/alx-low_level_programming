acde2dbcf8f6e2fa7ca2f671206a24f434406c35
kingsley <kingsleyba@gmail.com>
2023-08-07 06:18:21 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,118 +1,106 @@
 #include <stdlib.h>
 #include "main.h"
 
-/**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- *
- * Return: number of words
- */
-int count_word(char *s)
-{
-	int flag = 0, c, w = 0;
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
-	for (c = 0; s[c] != '\0'; c++)
+/**
+ * word_len - locates the index marking the end of the
+ * first word contained within a string
+ * @str: The string to be searched
+ *
+ * Return: The index marking the end of the initial word pointed to by str
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index] && str[index] != ' ')
 	{
-	if (s[c] == ' ')
-	flag = 0;
-	else if (flag == 0)
-	{
-	flag = 1;
-	w++;
+	len++;
+	index++;
 	}
-	}
-	return (w);
+	return (len);
 }
 
 /**
- * create_matrix - allocate memory for the matrix
- * @word_count: number of words
+ * count_words - counts the number of words contained within a string
+ * @str: The string to be searched
  *
- * Return: pointer to allocated matrix, NULL on failure
+ * Return: The number of words contained within str
  */
-char **create_matrix(int word_count)
+int count_words(char *str)
 {
-	char **matrix;
+	int index = 0, words = 0, len = 0;
 
-	matrix = (char **)malloc(sizeof(char *) * (word_count + 1));
-	return (matrix);
-}
-
-/**
- * extract_words - extract words from a string and populate the matrix
- * @str: input string
- * @matrix: matrix to populate
- */
-void extract_words(char *str, char **matrix)
-{
-	int i, word_index = 0, word_length = 0;
-	char *word_start = NULL;
-	char in_word = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[index])
 	{
-	if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-	{
-	if (!in_word)
-	{
-	in_word = 1;
-	word_start = &str[i];
-	word_length = 0;
+	len++;
+	index++;
 	}
-	word_length++;
+
+	index = 0;
+	while (index < len)
+	{
+	if (str[index] != ' ')
+	{
+	words++;
+	index += word_len(str + index);
 	}
 	else
 	{
-	if (in_word)
-	{
-	in_word = 0;
-
-	matrix[word_index] = (char *)malloc(sizeof(char) * (word_length + 1));
-	if (matrix[word_index] != NULL)
-	{
-	int j;
-
-	for (j = 0; j < word_length; j++)
-
-	{
-	matrix[word_index][j] = word_start[j];
-	}
-	matrix[word_index][j] = '\0';
-
-	word_index++;
+	index++;
 	}
 	}
-	}
-	}
-	matrix[word_index] = NULL;
+	return (words);
 }
 
 /**
  * strtow - splits a string into words
- * @str: string to split
+ * @str: The string to be split
  *
- * Return: pointer to an array of strings (success)
- * or NULL (error)
+ * Return: If str is NULL, empty, or the function fails - NULL,
+ *         otherwise - a pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
-	int word_count;
-	char **matrix;
+	char **strings;
+	int index = 0, words, w, letters, l;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || str[0] == '\0')
 	return (NULL);
 
-	word_count = count_word(str);
-
-	if (word_count == 0)
+	words = count_words(str);
+	if (words == 0)
 	return (NULL);
 
-	matrix = create_matrix(word_count);
-
-	if (matrix == NULL)
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
 	return (NULL);
 
-	extract_words(str, matrix);
+	for (w = 0; w < words; w++)
+	{
+	while (str[index] == ' ')
+	index++;
 
-	return (matrix);
+	letters = word_len(str + index);
+
+	strings[w] = malloc(sizeof(char) * (letters + 1));
+	if (strings[w] == NULL)
+	{
+	for (; w >= 0; w--)
+	free(strings[w]);
+
+	free(strings);
+	return (NULL);
+	}
+
+	for (l = 0; l < letters; l++)
+	strings[w][l] = str[index++];
+	strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
 }
