d57bd465174bb387365a50b58d4f597de56094c2
kingsley <kingsleyba@gmail.com>
2023-08-16 05:47:59 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Print name using pointer to function
+ * @name: String to print
+ * @f: Pointer to function for printing
+ *
+ * Return: Nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	return;
+	f(name);
+}
