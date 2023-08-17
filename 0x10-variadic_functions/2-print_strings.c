045a22b660625ec5abea9ac9f2cbbe2c0b9e6f26
kingsley <kingsleyba@gmail.com>
2023-08-17 09:45:12 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,9 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
-
 	for (i = 0; i < n; i++)
-
 	{
 	str = va_arg(list, char *);
 	if (!str)
