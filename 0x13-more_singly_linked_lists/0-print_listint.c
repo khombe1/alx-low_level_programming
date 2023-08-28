b0c31c2122c6f656349067743e45ca21219c9d14
khombe1 <kingsleybanda14@gmail.com>
2023-08-28 11:39:14 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,27 +1,18 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements
- * @h: linked list
+ * print_listint_reverse - prints all the elements of a linked list in reverse
+ * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint_reverse(const listint_t *h)
 {
 	if (h == NULL)
-	{
 	return (0);
-	}
 
-	size_t num = 0;
-	const listint_t *current = h;
+	size_t num = print_listint_reverse(h->next);
 
-	while (current != NULL)
-	{
-	printf("%d\n", current->n);
-	current = current->next;
-	num++;
-	}
-
-	return (num);
+	printf("%d\n", h->n);
+	return (num + 1);
 }
